e57f9e1a5aa2f71fb933014deb4b02e06ae060ec
ethan <hereisb612@gmail.com>
2025-10-01 22:39:29 -0400

    1

---
+++ src/main/TwoSum1.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.util.HashMap;
+
+public class TwoSum1 {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> set = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (set.containsKey(target - nums[i])) {
+                return new int[]{set.get(target - nums[i]), i};
+            }
+
+            set.put(nums[i], i);
+        }
+
+        return new int[]{};
+    }
+}
