d978b6f0f70216b723c1813b00a541a6dbf31f36
forty2 <hereisb612@gmail.com>
2025-12-25 23:05:06 -0500

    Implement maxOperations to count pairs summing to k

---
+++ lc1679.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int result = 0;
+
+        for (int num : nums) {
+            if (map.containsKey(k - num) && map.get(k - num) > 0) {
+                result++;
+                map.put(k - num, map.getOrDefault(k - num, 0) - 1);
+            } else {
+                map.put(num, map.getOrDefault(num, 0) + 1);
+            }
+        }
+
+        return result;
+    }
+}
